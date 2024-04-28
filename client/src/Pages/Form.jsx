827d94edf5a3ad52588481df58e43cca8c8ae2ba
shashirathee <154332412+shashirathee@users.noreply.github.com>
2024-04-28 11:12:37 +0530

    added enroll detail page and form page styling

--- client/src/Pages/Form.jsx
+++ client/src/Pages/Form.jsx
@@ -43,7 +43,7 @@ const EnrollDetail = () => {
 </form>
         <div>
             <label for="Techstack" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tech Stack used</label>
-            <input type="teckstack" id="Date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tech Stack" required />
+            <textarea type="teckstack" id="Date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tech Stack" required />
         </div>
     </div>
     <div class="mb-6">
@@ -56,7 +56,7 @@ const EnrollDetail = () => {
     </div> 
     <div class="mb-6">
         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Project Description</label>
-        <input type="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Project Description" required />
+        <textarea type="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Project Description" required />
     </div> 
    
     <div class="flex items-start mb-6">
--- client/src/Pages/ListingCompany.jsx
+++ client/src/Pages/ListingCompany.jsx
@@ -1,126 +1,195 @@
 import React from "react";
-// import { useLottie } from "lottie-react";
-// import Animation4 from "../files/Animation4.json";
+
 
 const Notification = () => {
-//   const options = {
-//     animationData: Animation4,
-//     style: {
-//       margin: "auto",
-//       width: "300px",
-//     },
-//   };
-//   const { View } = useLottie(options);
-  return (
-    <div className="h-screen bg-base-100  bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-     <div class="h-screen grid place-items-center my-8">
-        <div class="lg:w-2/5 sm:w-3/5 w-11/12 bg-gray-100 dark:bg-gray-800 rounded-xl mx-auto border p-10 shadow-sm">
-          <div class="inline-flex items-center justify-between w-full">
-            <h3 class="font-bold text-xl sm:text-2xl text-gray-800 dark:text-white">
-              Notifications
-            </h3>
-          </div>
-          <p class="mt-8 font-medium text-gray-500 text-sm sm:text-base dark:text-white">
-            New
-          </p>
 
-          <div class="mt-2 px-6 py-4 bg-white rounded-lg shadow w-full">
-            <div class=" inline-flex items-center justify-between w-full">
-              <div class="inline-flex items-center">
-                <img
-                  src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo-thumbnail.png"
-                  alt="Form Icon"
-                  class="w-10 h-10 mr-3"
+  return (
+    <div className="h-screen bg-base-100  ">
+     <div class="h-screen grid place-items-center my-8">
+     <section className="  p-10 flex flex-wrap items-center justify-evenly">
+            <div className="mt-2 mt-lg-auto">
+              <h1
+                href="#"
+                className="custom-link mb-2 text-6xl font-extrabold text-black  md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-gray-800 from-gray-600 font-hihi "
+              >
+                Chose your comfortable project!
+                <i className="bi-arrow-right ms-2"></i>
+              </h1>
+              {/* <Lottie style={style} animationData={Animation1} />
+               */}
+              {/* {View} */}
+            </div>
+          </section>{" "}
+<div class="container mx-auto bg-gray-50 min-h-screen p-8 antialiased">
+   <div >
+   
+      <div class="bg-gray-100 mx-auto  border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
+         <div class="flex p-3   border-l-8 border-red-600">
+         <div class="pb-1 md:pb-1 w-full md:w-40">
+          <img
+                  className="w-full hidden md:block"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
                 />
-                <h3 class="font-bold text-base text-gray-800">
-                  Udne ki website
-                </h3>
-              </div>
-              <p class="text-xs text-gray-500">12:47</p>
-            </div>
-            <div className="">
-              <p class="mt-1 text-sm">You are invited by sujigya</p>
-              <button className="btn btn-info mx-6 my-2 p-3 bg-cyan-300 text-black rounded-full">
-                Accept
-              </button>
-              <button className="btn btn-info p-3 bg-red-400 text-black rounded-full">
-                Reject
-              </button>
-            </div>
-          </div>
-          <div class="mt-2 px-6 py-4 bg-white rounded-lg shadow w-full">
-            <div class=" inline-flex items-center justify-between w-full">
-              <div class="inline-flex items-center">
                 <img
-                  src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo-thumbnail.png"
-                  alt="Form Icon"
-                  class="w-10 h-10 mr-3"
+                  className="w-full md:hidden"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
                 />
-                <h3 class="font-bold text-base text-gray-800">
-                  Udne ki website
-                </h3>
-              </div>
-              <p class="text-xs text-gray-500">12:47</p>
-            </div>
-            <div className="">
-              <p class="mt-1 text-sm">You are invited by sujigya</p>
-              <button className="btn btn-info mx-6 my-2 p-3 bg-cyan-300 text-black rounded-full">
-                Accept
-              </button>
-              <button className="btn btn-info p-3 bg-red-400 text-black rounded-full">
-                Reject
-              </button>
-            </div>
           </div>
-          <div class="mt-2 px-6 py-4 bg-white rounded-lg shadow w-full">
-            <div class=" inline-flex items-center justify-between w-full">
-              <div class="inline-flex items-center">
+            <div class="flex-1">
+               <div class="ml-3 space-y-1 border-r-2 pr-3">
+                  <div class="text-base leading-6 font-normal">UDNE KI WEBSITE</div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500"> Google</span> </div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500">Description</span> This project is based on AI modelling and rendering of objects</div>
+               </div>
+            </div>
+            <div class="border-r-2 pr-3">
+               <div >
+                  <div class="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
+                     <div class="uppercase text-xs leading-4 font-medium">Enrolled</div>
+                     <div class="text-center text-sm leading-4 font-semibold text-gray-800">62</div>
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="ml-3 my-5 bg-red-600 p-1 w-20">
+                  <div class="uppercase text-xs leading-4 font-semibold text-center text-red-100">Apply</div>
+               </div>
+            </div>
+            
+         </div>
+      </div>
+      
+   </div>
+      <p>‎</p>
+   <div >
+   
+      <div class="bg-gray-100 mx-auto  border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
+         <div class="flex p-3   border-l-8 border-red-600">
+         <div class="pb-1 md:pb-1 w-full md:w-40">
+          <img
+                  className="w-full hidden md:block"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
+                />
                 <img
-                  src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo-thumbnail.png"
-                  alt="Form Icon"
-                  class="w-10 h-10 mr-3"
+                  className="w-full md:hidden"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
                 />
-                <h3 class="font-bold text-base text-gray-800">
-                  Udne ki website
-                </h3>
-              </div>
-              <p class="text-xs text-gray-500">12:47</p>
-            </div>
-            <div className="">
-              <p class="mt-1 text-sm">You are invited by sujigya</p>
-              <button className="btn btn-info mx-6 my-2 p-3 bg-cyan-300 text-black rounded-full">
-                Accept
-              </button>
-              <button className="btn btn-info p-3 bg-red-400 text-black rounded-full">
-                Reject
-              </button>
-            </div>
           </div>
-          <div class="mt-2 px-6 py-4 bg-white rounded-lg shadow w-full">
-            <div class=" inline-flex items-center justify-between w-full">
-              <div class="inline-flex items-center">
+            <div class="flex-1">
+               <div class="ml-3 space-y-1 border-r-2 pr-3">
+                  <div class="text-base leading-6 font-normal">UDNE KI WEBSITE</div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500"> Google</span> </div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500">Description</span> This project is based on AI modelling and rendering of objects</div>
+               </div>
+            </div>
+            <div class="border-r-2 pr-3">
+               <div >
+                  <div class="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
+                     <div class="uppercase text-xs leading-4 font-medium">Enrolled</div>
+                     <div class="text-center text-sm leading-4 font-semibold text-gray-800">62</div>
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="ml-3 my-5 bg-red-600 p-1 w-20">
+                  <div class="uppercase text-xs leading-4 font-semibold text-center text-red-100">Apply</div>
+               </div>
+            </div>
+            
+         </div>
+      </div>
+      
+   </div>
+      <p>‎</p>
+   <div >
+   
+      <div class="bg-gray-100 mx-auto  border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
+         <div class="flex p-3   border-l-8 border-red-600">
+         <div class="pb-1 md:pb-1 w-full md:w-40">
+          <img
+                  className="w-full hidden md:block"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
+                />
                 <img
-                  src="https://w7.pngwing.com/pngs/326/85/png-transparent-google-logo-google-text-trademark-logo-thumbnail.png"
-                  alt="Form Icon"
-                  class="w-10 h-10 mr-3"
+                  className="w-full md:hidden"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
                 />
-                <h3 class="font-bold text-base text-gray-800">
-                  Udne ki website
-                </h3>
-              </div>
-              <p class="text-xs text-gray-500">12:47</p>
-            </div>
-            <div className="">
-              <p class="mt-1 text-sm">You are invited by sujigya</p>
-              <button className="btn btn-info mx-6 my-2 p-3 bg-cyan-300 text-black rounded-full">
-                Accept
-              </button>
-              <button className="btn btn-info p-3 bg-red-400 text-black rounded-full">
-                Reject
-              </button>
-            </div>
           </div>
-        </div>
+            <div class="flex-1">
+               <div class="ml-3 space-y-1 border-r-2 pr-3">
+                  <div class="text-base leading-6 font-normal">UDNE KI WEBSITE</div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500"> Google</span> </div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500">Description</span> This project is based on AI modelling and rendering of objects</div>
+               </div>
+            </div>
+            <div class="border-r-2 pr-3">
+               <div >
+                  <div class="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
+                     <div class="uppercase text-xs leading-4 font-medium">Enrolled</div>
+                     <div class="text-center text-sm leading-4 font-semibold text-gray-800">62</div>
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="ml-3 my-5 bg-red-600 p-1 w-20">
+                  <div class="uppercase text-xs leading-4 font-semibold text-center text-red-100">Apply</div>
+               </div>
+            </div>
+            
+         </div>
+      </div>
+      
+   </div>
+      <p>‎</p>
+   <div >
+   
+      <div class="bg-gray-100 mx-auto  border-gray-500 border rounded-sm  text-gray-700 mb-0.5">
+         <div class="flex p-3   border-l-8 border-red-600">
+         <div class="pb-1 md:pb-1 w-full md:w-40">
+          <img
+                  className="w-full hidden md:block"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
+                />
+                <img
+                  className="w-full md:hidden"
+                  src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/image8-2.jpg?width=595&height=400&name=image8-2.jpg"
+                  alt="dress"
+                />
+          </div>
+            <div class="flex-1">
+               <div class="ml-3 space-y-1 border-r-2 pr-3">
+                  <div class="text-base leading-6 font-normal">UDNE KI WEBSITE</div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500"> Google</span> </div>
+                  <div class="text-sm leading-4 font-normal"><span class="text-xs leading-4 font-normal text-gray-500">Description</span> This project is based on AI modelling and rendering of objects</div>
+               </div>
+            </div>
+            <div class="border-r-2 pr-3">
+               <div >
+                  <div class="ml-3 my-3 border-gray-200 border-2 bg-gray-300 p-1">
+                     <div class="uppercase text-xs leading-4 font-medium">Enrolled</div>
+                     <div class="text-center text-sm leading-4 font-semibold text-gray-800">62</div>
+                  </div>
+               </div>
+            </div>
+            <div>
+               <div class="ml-3 my-5 bg-red-600 p-1 w-20">
+                  <div class="uppercase text-xs leading-4 font-semibold text-center text-red-100">Apply</div>
+               </div>
+            </div>
+            
+         </div>
+      </div>
+      
+   </div>
+      <p>‎</p>
+</div>
       </div>
     </div>
   );
